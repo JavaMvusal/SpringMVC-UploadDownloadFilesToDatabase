7120c6711de84979e4c57b850f3a9776f268d10c
Vusal Mustafayev <114743695+JavaMvusal@users.noreply.github.com>
2022-11-18 22:50:50 +0400

    Add files via upload

---
+++ DocumentManager/src/test/java/net/javacoding/DocumentManager/DocumentManagerApplicationTests.java
@@ -0,0 +1,43 @@
+package net.javacoding.DocumentManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Date;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class DocumentManagerApplicationTests {
+	@Autowired
+	private DocumentRepository repo;
+	@Autowired
+	private TestEntityManager testEntityManager;
+	@Test
+	@Rollback(value = false)
+	void testInsertDocument() throws IOException {
+		File file = new File("C:\\Users\\user\\Downloads\\JavaDeveloper.doc");
+		Document document = new Document();
+		document.setName(file.getName());
+		byte[] bytes =Files.readAllBytes(file.toPath());
+		document.setContent(bytes);
+		long fileSize = bytes.length;
+		document.setSize(fileSize);
+		document.setUploadTime(new Date());
+		Document savedDoc = repo.save(document);
+		Document existDoc = testEntityManager.find(Document.class,savedDoc.getId());
+		assertThat(existDoc.getSize()).isEqualTo(fileSize);
+
+	}
+
+}
